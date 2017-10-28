8e4b1783b6316e68c13c607988c9bd240ad0b9da
Riley Johnson <rileyjohnson99@hotmail.com>
2017-10-27 19:18:39 -0500

    Added some basic structure as to what the final version will eventually look like

--- codegolfify.c
+++ codegolfify.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
-int main()  {
-    //There will be stuff here
+int main(int argc, char **argv) {
+    char c;
+    int head;
+    FILE *ifi, *ofo;
+
+    if(argc < 3){
+        ifi = stdin;
+        ofo = stdout;
+    } else {
+        ifi = fopen(argv[1], "r");
+        ofo = fopen(argv[2], "r");
+    }
+    
+    while((c = getc(ifi))){
+        if(head){
+
+        } else {
+
+        }
+    }
 }
